27a9c145034ee97922ce7f7d096e7818da26a042
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-05-13 19:56:07 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+
+        result = []
+        for candidate in word_list:
+            if sorted(candidate) == sorted_word and candidate.lower() != self.word.lower():
+                result.append(candidate)
+
+        return result
+
+
